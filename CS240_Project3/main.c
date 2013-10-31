29b87e59483b24c578a0df39905945693f573bc8
Tommy McArdle <mcardle@purdue.edu>
2013-10-31 16:15:52 -0400

    Initial Commit

---
+++ CS240_Project3/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CS240_Project3
+//
+//  Created by Tommy McArdle on 10/31/13.
+//  Copyright (c) 2013 Tommy McArdle. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
