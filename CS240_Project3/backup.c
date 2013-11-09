80ca0e71f41babef7137cef4d711fd70b8b9b10c
Tommy McArdle <mcardle@purdue.edu>
2013-11-08 20:49:00 -0500

    v1.0
    First version of the log file.
    
    AutoGrader Score: 24%

--- CS240_Project3/backup.c
+++ CS240_Project3/backup.c
@@ -1,5 +1,5 @@
 /**
- * Project 3 - Directory Backup
+ * Project 3 backup.c (C89)
  *
  * @author Tom McArdle
  *
@@ -11,8 +11,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>	// directories library
-#include <dirent.h>		// directories library 
+#include <sys/types.h>	/* directories library */
+#include <dirent.h>		/* directories library  */
+#include <sys/dir.h>
 #include "backup.h"
 #include <sys/stat.h>
 #include <time.h>
@@ -20,52 +21,55 @@
 /*------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------------------*/
 
+char writeDirToFile(FILE *fLN, char *cSD);
+char *removeLastChar (char *str);
 char *getType(unsigned char t);
 
-int main(int argc, char * argv[]){
-	
 
-	
-	
-	DIR *dSD = NULL;					// source directory
-	DIR *dDD = NULL;					// destination directory
+int main(int argc, char * argv[]){
+	DIR *dSD = NULL;					/* source directory */
+	DIR *dDD = NULL;					/* destination directory */
 	long maxBackups = 0;
-	char srcDirName[FILENAME_MAX];		// source directory name
-	char *pSDN = &srcDirName[0];	// pointer to the source directory name
-	char destDirName[FILENAME_MAX];		// destination directory name
-	char *pDDN = &destDirName[0];	// pointer to the destination directory name
+	char srcDirName[FILENAME_MAX];		/* source directory name */
+	char *pSDN = &srcDirName[0];		/* pointer to the source directory name */
+	char destDirName[FILENAME_MAX];		/* destination directory name */
+	char *pDDN = &destDirName[0];		/* pointer to the destination directory name */
+	int i = 0;
+	
 	/*---------------------------------ARGS-------------------------------------------------------*/
-	// Search the arguments for source directory (-s), destination directory (-d), and maximum
-	// number of backup instances (-m)
-	for (int i=0; i<argc; i++){
+	/**
+	 * Search the arguments for source directory (-s), destination directory (-d), and maximum
+	 * number of backup instances (-m)
+	 */
+	for (i=0; i<argc; i++){
 #ifdef debug_args
 		printf("arg (%d): %s\n", i, argv[i]);
 #endif
-		// (required) Search for sorce directory
+		/* (required) Search for sorce directory */
 		if (strcmp(argv[i], "-s") == 0){
 			pSDN = argv[i+1];
 		}
 		
-		// (optional) Search for destination directory
+		/* (optional) Search for destination directory */
 		if (strcmp(argv[i], "-d") == 0){
 			pDDN = argv[i+1];
 		}
 		
-		// (optional) Maximum number of backup instances to save
+		/* (optional) Maximum number of backup instances to save */
 		if (strcmp(argv[i], "-m") == 0){
 			maxBackups = atol(argv[i+1]);
 		}
 	}
 
-	if (maxBackups == 0)					// If the maximum number of backups were not defined,
-		maxBackups = DEFAULT_MAX_BACKUPS;	// then the default value is used.
+	if (maxBackups == 0)					/* If the maximum number of backups were not defined, */
+		maxBackups = DEFAULT_MAX_BACKUPS;	/* then the default value is used. */
 
-	if (pDDN == NULL){	// If destination dierectory name not defined
-		pDDN = DEFAULT_DEST_DIR;		// then define the destination name to the default name
-		dDD = opendir(pDDN);	// and open the default directory
+	if (pDDN == NULL){				/* If destination dierectory name not defined */
+		pDDN = DEFAULT_DEST_DIR;	/* then define the destination name to the default name */
+		dDD = opendir(pDDN);		/* and open the default directory */
 	}
 	else{
-		dDD = opendir(pDDN);	// else open the directory defined in the arguments
+		dDD = opendir(pDDN);		/* else open the directory defined in the arguments */
 	}
 	
 #ifdef debug_args
@@ -112,47 +116,58 @@ int main(int argc, char * argv[]){
  * @param	char *logFilePath	destination directory name used to store the logfile
  */
 void createLog(char *sourceDir, char *logFilePath){
-//	FILE* logLast = fopen(LOG_LAST_FILENAME, "w+");
-	strcat(logFilePath, "/");					// prepares the file path for a new file path
-	strcat(logFilePath, LOG_NEW_FILENAME);		// adds new log filename to file path
-	FILE* logNew = fopen(logFilePath, "w+");	// opens files for writing and reading
-	struct dirent *ep;							// dirent structure created
-	DIR *srcDir = opendir(sourceDir);			// creating directory based on the sourceDir name passed into createLog()
+	strcat(logFilePath, "/");					/* prepares the file path for a new file path */
+	strcat(logFilePath, LOG_NEW_FILENAME);		/* adds new log filename to file path */
+	FILE *logNew = fopen(logFilePath, "w+");	/* opens files for writing and reading */
+//	DIR *srcDir = opendir(sourceDir);			/* creating directory based on the sourceDir name
+//												 * passed into createLog() */
+	writeDirToFile(logNew, sourceDir);
 	
-	
-	char *ct = malloc(25);
-	char *mt = malloc(25);
+	fclose(logNew);		/* close logNew file */
+//	closedir (srcDir);	/* close srcDir file */
+}
+
+
+char writeDirToFile(FILE *fLN, char *cSD){
 	struct stat *st = malloc(sizeof(struct stat));
-
-//	ep = readdir (srcDir);
 	struct dirent **nameList;
-    int count = scandir(sourceDir, &nameList, NULL, alphasort);
+    int count = scandir(cSD, &nameList, NULL, alphasort);
 	while(count--){
-		stat(nameList[count]->d_name, st);	// create statistics for directory
-		char *c = ctime(&(st->st_ctime));	// get creation time
-		strncpy(ct, c, strlen(c)-1);		// remove '\n' from creation time
-		char *m = ctime(&(st->st_mtime));	// get modification time
-		strncpy(mt, m, strlen(m)-1);		// remove '\n' from creation time
+		stat(nameList[count]->d_name, st);	/* create statistics for directory */
 		
-		// prints data type, data size, creation time, last modification time, and data name to log file
-		fprintf(logNew, "%s\t%zd\t%s\t%s\t%s\n",
-				getType(nameList[count]->d_type),	// DataType
-				st->st_size,						// DataSizeInBytes
-				ct,									// CreationTimeStamp
-				mt,									// LastModificationTimeStamp
-				nameList[count]->d_name);			// DataName
-
+		/* prints data type, data size, creation time, last modification time, and data name to log file */
+		fprintf(fLN, "%s\t%ld\t%s\t%s\t%s\n",
+				getType(nameList[count]->d_type),		/* DataType */
+				(long)st->st_size,						/* DataSizeInBytes */
+				removeLastChar(ctime(&(st->st_ctime))),	/* CreationTimeStamp */
+				removeLastChar(ctime(&(st->st_mtime))),	/* ModificationTimeStamp */
+				nameList[count]->d_name);				/* DataName */
+		
 		free(nameList[count]);
 	}
 	free(st);
 	free(nameList);
-	free(ct);
-	free(mt);
-	fclose(logNew);		// close logNew file
-	closedir (srcDir);	// close srcDir file 
+	return 0;
+}
+
+/**
+ * @function	char *removeLastChar (char str[])
+ * @details		Replaces the last character of a string with '\0', indicating end of string
+ * @param	char str[]	Character array
+ * @return	char *str	Character pointer of the string passed in with the last char removed
+ */
+char *removeLastChar (char str[]){
+	str[strlen(str)-1] = '\0';
+	return str;
 }
 
 
+/**
+ * @function	char *getType(unsigned char t)
+ * @details		Converts the data type hexidecimal value to a string of the #define type
+ * @param	unsigned char t		Hexidecimal value passed in to be converted to string
+ * @return	char *				String of data type
+ */
 char *getType(unsigned char t){
 	switch (t){
 		case DT_UNKNOWN: return "DT_UNKNOWN";
@@ -186,4 +201,4 @@ char *getType(unsigned char t){
 
 
 
-// Space used to keep code of bottom of screen. It bugs me.
+/* Space used to keep code of bottom of screen. It bugs me. */
